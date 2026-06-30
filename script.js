0e6b33171b38583b825372244afc4c07cdf76539
phoutsaissp <phoutsaissp@gmail.com>
2026-06-30 22:48:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent="Light Mode";
+    }
+    else{
+        themeToggle.textContent="Dark Mode";
+    }
+    });
